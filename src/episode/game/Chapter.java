9e9482c6c9255d78865f00c27aff7ce0cd2c84d4
fr1enda <fr1enda@163.com>
2022-12-04 21:07:40 +0800

    副本创建

---
+++ src/episode/game/Chapter.java
@@ -0,0 +1,12 @@
+package episode.game;
+
+import character.game.Player;
+
+public class Chapter {
+    private int chapterNum=1;
+    private int levelNum=1;
+    public Chapter(int chapterNum, int levelNum){
+        this.chapterNum=chapterNum;
+        this.levelNum=levelNum;
+    }
+}
---
+++ src/episode/game/LevelData.java
@@ -0,0 +1,6 @@
+package episode.game;
+
+public class LevelData {
+    static Chapter chapters[]=new Chapter[3];
+    static Level levels[]=new Level[8];
+}
